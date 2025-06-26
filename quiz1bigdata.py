650e995aff0a5ae3777efb0925245b76de0caae2
kwizera06 <151912656+kwizera06@users.noreply.github.com>
2025-06-26 23:24:22 +0300

    Update quiz1bigdata.py

--- quiz1bigdata.py
+++ quiz1bigdata.py
@@ -1,10 +1,12 @@
-def calculate_age(year_of_birth, current_year=2025):
-    return current_year - year_of_birth
+def calculate_age(year_of_birth):
+    current_year = 2025
+   return current_year - year_of_birth
 
-name = input("Please enter your name: ")
-year_of_birth = int(input("Please enter your year of birth (e.g., 2000): "))
-age = calculate_age(year_of_birth)
+ name = input("Enter your name: ")
 
-print("Thank you")
-print(f"{name}, your age is: {age}")
-print("Current date: Wednesday, June 25, 2025, 7:23 PM SAST")
+ year_of_birth = int(input("Enter your year of birth (e.g., 2000): "))
+
+ age = calculate_age(year_of_birth)
+
+ print("Thank you")
+ print(f"{name}, You are age is:{age} ")
